022f1cc1cfdfc19ed07206a709a06b1ca668fc57
Brendan Slabe <slabe@google.com>
2025-05-05 22:55:26 +0000

    fix __init__

--- inference_perf/client/storage/__init__.py
+++ inference_perf/client/storage/__init__.py
@@ -11,22 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-<<<<<<<< HEAD:inference_perf/client/storage/__init__.py
+
 from .base import StorageClient
 from .gcs import GoogleCloudStorageClient
-========
->>>>>>>> 9afd930bf7a97b1014ba1a425fabf71705ddfe4a:inference_perf/metrics/observed.py
 
-from typing import List
-from inference_perf.metrics.base import MetricsSource
-from inference_perf.config import MetricsConfig, RequestMetric
-
-<<<<<<<< HEAD:inference_perf/client/storage/__init__.py
 __all__ = ["StorageClient", "GoogleCloudStorageClient"]
-========
-class ObservedMetricsCollector(MetricsSource):
-    def __init__(self, config: MetricsConfig) -> None:
-        self.config = config
-        self.metrics: List[RequestMetric] = []
-        pass
->>>>>>>> 9afd930bf7a97b1014ba1a425fabf71705ddfe4a:inference_perf/metrics/observed.py
