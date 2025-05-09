722cdbdf0f69f6aaa808f93bd5afa71259b6e659
Brendan Slabe <slabe@google.com>
2025-05-09 21:51:57 +0000

    strange check

--- inference_perf/main.py
+++ inference_perf/main.py
@@ -72,7 +72,7 @@ def main_cli() -> None:
     # Define Report Generator
     reportgen = ReportGenerator(
         client_request_metrics_collector=vllm_client.prompt_metrics_collector,
-        prometheus_metrics_collector=vllm_client.prometheus_collector,
+        prometheus_metrics_collector=vllm_client.prometheus_collector if vllm_client.prometheus_collector else None,
     )
 
     # Define Storage Clients
