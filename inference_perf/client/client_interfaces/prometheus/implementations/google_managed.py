ec404fcaab00b44454801abc152bb4a448e967f0
Brendan Slabe <slabe@google.com>
2025-05-12 22:56:42 +0000

    missing files

---
+++ inference_perf/client/client_interfaces/prometheus/implementations/google_managed.py
@@ -0,0 +1,61 @@
+
+from typing import Any, List, Optional
+
+import requests
+from inference_perf.client.client_interfaces.prometheus.base import PrometheusMetricsCollector
+from inference_perf.client.client_interfaces.prometheus.prometheus_metrics import PrometheusMetric
+from inference_perf.config import PrometheusCollectorConfig
+import google.auth
+import google.auth.transport.requests
+
+
+class GMPMetricsCollector(PrometheusMetricsCollector):
+    project_id: str
+    credentials: Any
+
+    def __init__(self, metrics: List[PrometheusMetric], config: PrometheusCollectorConfig):
+        super().__init__(metrics=metrics, config=config)
+        credentials, project_id = google.auth.default()
+        self.credentials = credentials
+        self.project_id = project_id
+        self.url='https://monitoring.googleapis.com/v1/projects/%s/location/global/prometheus/api/v1/metadata' % (self.project_id)
+
+    async def query_metric(self, query: str, duration: float) -> Optional[float]:
+        auth_req = google.auth.transport.requests.Request()
+        self.credentials.refresh(auth_req)
+        
+        headers_api = {'Authorization': 'Bearer ' + self.credentials.token}
+        params = {'query': query}
+        request_post = requests.get(url=self.url, headers=headers_api)
+        all_metrics_metadata = request_post.json()
+        if request_post.ok is not True:
+            print("HTTP Error: %s" % (all_metrics_metadata))
+            return None
+        if all_metrics_metadata["status"] != "success":
+            print("Metadata error response: %s" % all_metrics_metadata["error"])
+            return None
+
+        print(f"Evaluating query: {query}")
+        request_post = requests.get(url=self.url, headers=headers_api, params=params)
+        response = request_post.json()
+
+        print(f"Got response from metrics server: {response}")
+        if request_post.ok:
+            if response["status"] == "success" and response["data"] and response["data"]["result"]:
+                r = response["data"]["result"]
+                if not r:
+                    print(f"Failed to get result for {query}")
+                    return None
+                v = r[0].get("value", None)
+                if not v:
+                    print(f"Failed to get value for result: {r}")
+                    return None
+                print(f"Result for {query}: {v[1]}")
+                return float(v[1])
+            else:
+                print("Cloud Monitoring PromQL Error: %s" % (response))
+                return None
+        else:
+            print("HTTP Error: %s" % (response))
+            return None
+            
---
+++ inference_perf/client/client_interfaces/prometheus/implementations/self_hosted.py
@@ -0,0 +1,74 @@
+
+from typing import List, Optional
+import requests
+from inference_perf.client.client_interfaces.prometheus.base import PrometheusMetricsCollector
+from inference_perf.client.client_interfaces.prometheus.prometheus_metrics import PrometheusMetric
+from inference_perf.config import PrometheusCollectorConfig
+
+
+class SelfHostedPrometheusMetricsCollector(PrometheusMetricsCollector):
+    def __init__(self, metrics: List[PrometheusMetric], config: PrometheusCollectorConfig):
+        super().__init__(metrics=metrics, config=config)
+        pass
+
+    async def query_metric(self, query: str, duration: float) -> Optional[float]:
+        """
+        Executes the given query on the Prometheus server and returns the result.
+
+        Args:
+        query: the PromQL query to execute
+
+        Returns:
+        The result of the query.
+        """
+        query_result = 0.0
+        try:
+            response = requests.get(f"{self.url}/api/v1/query", params={"query": query, "time": str(duration)})
+            if response is None:
+                print("Error executing query: %s" % (query))
+                return query_result
+
+            response.raise_for_status()
+        except Exception as e:
+            print("Error executing query: %s" % (e))
+            return query_result
+
+        # Check if the response is valid
+        # Sample response:
+        # {
+        #     "status": "success",
+        #     "data": {
+        #         "resultType": "vector",
+        #         "result": [
+        #             {
+        #                 "metric": {},
+        #                 "value": [
+        #                     1632741820.781,
+        #                     "0.0000000000000000"
+        #                 ]
+        #             }
+        #         ]
+        #     }
+        # }
+        response_obj = response.json()
+        if response_obj.get("status") != "success":
+            print("Error executing query: %s" % (response_obj))
+            return query_result
+
+        data = response_obj.get("data", {})
+        result = data.get("result", [])
+        if len(result) > 0 and "value" in result[0]:
+            if isinstance(result[0]["value"], list) and len(result[0]["value"]) > 1:
+                # Return the value of the first result
+                # The value is in the second element of the list
+                # e.g. [1632741820.781, "0.0000000000000000"]
+                # We need to convert it to float
+                # and return it
+                # Convert the value to float
+                try:
+                    query_result = float(result[0]["value"][1])
+                except ValueError:
+                    print("Error converting value to float: %s" % (result[0]["value"][1]))
+                    return query_result
+        return query_result
+
